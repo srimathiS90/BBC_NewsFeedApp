88faf469af738444505b212af3e673fa673ebe0b
Srimathi Sathyanarayanan <srimathisathya90@gmail.com>
2020-01-22 23:47:46 -0800

    Updates

---
+++ precache-manifest.c843029b8069612db62b9407f6171d1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11983613c97f72912808d58d7110bcd8",
+    "url": "/BBC_NewsFeedApp/index.html"
+  },
+  {
+    "revision": "942593087fa805097860",
+    "url": "/BBC_NewsFeedApp/static/css/main.3d3c7dd5.chunk.css"
+  },
+  {
+    "revision": "7d1b35359b6a9a879587",
+    "url": "/BBC_NewsFeedApp/static/js/2.46fa73bb.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/BBC_NewsFeedApp/static/js/2.46fa73bb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "942593087fa805097860",
+    "url": "/BBC_NewsFeedApp/static/js/main.75a65ad1.chunk.js"
+  },
+  {
+    "revision": "8e9dc8526476bba61117",
+    "url": "/BBC_NewsFeedApp/static/js/runtime-main.129df817.js"
+  }
+]);
